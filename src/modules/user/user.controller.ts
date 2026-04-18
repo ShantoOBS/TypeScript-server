9604161a0da6ee1ae94ab1aaa6230ec4123cba3b
ShantoOBS <roys22127@gmail.com>
2026-04-18 18:40:33 +0600

    adding user routes , controller and services

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,139 @@
+import {Request,Response} from "express"
+import {userServices} from './user.service'
+
+const createUser= async(req:Request ,res: Response)=>{
+    const {email,name}=req.body;
+    
+    try{
+
+     const result=await userServices.createUser(name,email);
+        res.status(200).json({
+        success : true,
+        message : "Data is inserted",
+        data : result.rows[0],
+    })
+
+    }catch(err:any){
+       
+        res.status(500).json({
+          success : false,
+          message : err.message
+        })
+        
+    }
+
+}
+
+const getAllUser=async(req:Request , res:Response)=>{
+   
+  try{
+
+    const result=await userServices.getAllUser();
+    res.status(200).json({
+      success : true,
+      message : "User retrieved successfully",
+      data : result.rows,
+    })
+
+  }catch(err:any){
+     res.status(500).json({
+      success : false,
+      message : err.message
+     })
+  }
+
+}
+
+const getSingleUser=async(req:Request, res:Response)=>{
+  
+   try{
+
+    const result=await userServices.getSingleUser(req.params.id as string);
+
+    if(result.rows.length==0){
+        res.status(400).json({
+      success : false,
+      message : "Data not found"
+     })
+    }
+    
+    res.status(200).json({
+      success : true,
+      message : "User retrieved successfully",
+      data : result.rows,
+    })
+
+   }catch(err:any){
+     res.status(500).json({
+      success : false,
+      message : err.message
+     })
+    }
+
+}
+
+const updateUser=async(req:Request, res:Response)=>{
+
+  const {email,name}=req.body;
+  
+   try{
+
+    const result=await userServices.updateUser(name,email,req.params.id as string);
+
+    if(result.rows.length==0){
+        res.status(400).json({
+      success : false,
+      message : "Data not found"
+     })
+    }
+    
+    res.status(200).json({
+      success : true,
+      message : "User update successfully",
+      data : result.rows,
+    })
+
+   }catch(err:any){
+     res.status(500).json({
+      success : false,
+      message : err.message
+     })
+    }
+
+}
+
+const deleteUser=async(req:Request, res:Response)=>{
+  
+   try{
+
+    const result=await userServices.deleteUser(req.params.id as string);
+
+    if(result.rowCount==0){
+        res.status(400).json({
+      success : false,
+      message : "Data not found"
+     })
+    }
+    
+    res.status(200).json({
+      success : true,
+      message : "User delete successfully",
+      data : result.rows,
+    })
+
+   }catch(err:any){
+     res.status(500).json({
+      success : false,
+      message : err.message
+     })
+    }
+
+}
+
+export const userContorllers = {
+     createUser,
+     getAllUser,
+     getSingleUser,
+     updateUser,
+     deleteUser
+} 
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,50 +1,15 @@
 import express, {Request,Response} from "express"
-import { pool } from "../../config/db";
 const router= express.Router();
+import { userContorllers } from "./user.controller"
 
-router.post("/",async(req:Request ,res: Response)=>{
-    const {email,name}=req.body;
-    
-    try{
+router.post("/",userContorllers.createUser);
 
-     const result= await pool.query(`
-           INSERT INTO users(name,email) VALUES($1, $2) RETURNING *`,[name,email]);
-        res.status(200).json({
-        success : true,
-        message : "Data is inserted",
-        data : result.rows[0],
-    })
+router.get("/",userContorllers.getAllUser);
 
-    }catch(err:any){
-       
-        res.status(500).json({
-          success : false,
-          message : err.message
-        })
-        
-    }
+router.get("/:id",userContorllers.getSingleUser);
 
-})
+router.put("/:id",userContorllers.updateUser);
 
-
-router.get("/",async(req:Request , res:Response)=>{
-   
-  try{
-
-    const result= await pool.query(`SELECT * FROM users`);
-    res.status(200).json({
-      success : true,
-      message : "User retrieved successfully",
-      data : result.rows,
-    })
-
-  }catch(err:any){
-     res.status(500).json({
-      success : false,
-      message : err.message
-     })
-  }
-
-})
+router.delete("/:id",userContorllers.deleteUser);
 
 export const userRoutes = router;
